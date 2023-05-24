bd4abf007827a8ca11822039b7704c0b50cc9774
Suhyun Kim <ideptor@gmail.com>
2023-05-24 21:48:36 +0900

    resize image

--- resize.py
+++ resize.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 import os
-import pathlib
 from tqdm import tqdm
+from PIL import Image
 
 
 
@@ -17,14 +17,21 @@ def get_args() -> dict:
 
     return parser.parse_args()
 
+
 def resize(src: str, dst: str, size: int):
-    pbar = tqdm(os.listdir(src))
-    for label in pbar:
+
+    for label in os.listdir(src):
+        pbar = tqdm(os.listdir(f'{src}/{label}'))
         os.makedirs(f'{dst}/{label}', exist_ok=True)
-        for image in os.listdir(f'{src}/{label}'):
-            img_src_path = f'{src}/{label}/{image}'
-            img_dst_path = f'{dst}/{label}/{image}'.replace('.jpeg', '.jpg')
-            pbar.set_description(f'{img_src_path}>{img_dst_path}')
+        for image in pbar:
+            try:
+                img_src_path = f'{src}/{label}/{image}'
+                img_dst_path = f'{dst}/{label}/{image}'.replace('.jpeg', '.jpg')
+                pbar.set_description(f'{img_src_path}>{img_dst_path}')
+                Image.open(img_src_path).resize((size,size)).save(img_dst_path)
+            except OSError as e:
+                print(e, ' < ', img_src_path)
+        pbar.close()
 
 if __name__ == '__main__':
     args = get_args()
