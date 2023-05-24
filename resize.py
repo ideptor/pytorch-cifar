fe64189bb5d032b20081ff04b7d19207a333f63e
Suhyun Kim <ideptor@gmail.com>
2023-05-24 21:42:48 +0900

    add progress

--- resize.py
+++ resize.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 import os
 import pathlib
+from tqdm import tqdm
+
 
 
 def get_args() -> dict:
@@ -16,11 +18,13 @@ def get_args() -> dict:
     return parser.parse_args()
 
 def resize(src: str, dst: str, size: int):
-    for label in os.listdir(src):
+    pbar = tqdm(os.listdir(src))
+    for label in pbar:
+        os.makedirs(f'{dst}/{label}', exist_ok=True)
         for image in os.listdir(f'{src}/{label}'):
             img_src_path = f'{src}/{label}/{image}'
             img_dst_path = f'{dst}/{label}/{image}'.replace('.jpeg', '.jpg')
-            print(img_src_path, ">", img_dst_path, size)
+            pbar.set_description(f'{img_src_path}>{img_dst_path}')
 
 if __name__ == '__main__':
     args = get_args()
