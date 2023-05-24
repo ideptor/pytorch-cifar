ac314917871fc00bf587c26f997ec60b4642cda1
Suhyun Kim <ideptor@gmail.com>
2023-05-24 22:49:58 +0900

    train test split

--- resize.py
+++ resize.py
@@ -1,7 +1,9 @@
 from argparse import ArgumentParser
 import os
+import random
 from tqdm import tqdm
 from PIL import Image
+import shutil
 
 
 def get_args() -> dict:
@@ -17,23 +19,35 @@ def get_args() -> dict:
     return parser.parse_args()
 
 
-def resize(src: str, dst: str, size: int):
+def prepare_train_test_images(src: str, dst: str, size: int, train_ratio: float=.8):
+
+    images = list()
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
 
     for label in os.listdir(src):
-        pbar = tqdm(os.listdir(f'{src}/{label}'))
-        os.makedirs(f'{dst}/{label}', exist_ok=True)
-        for image in pbar:
-            try:
-                img_src_path = f'{src}/{label}/{image}'
-                img_dst_path = f'{dst}/{label}/{image}'.replace('.jpeg', '.jpg')
-                pbar.set_description(f'{img_src_path}>{img_dst_path}')
-                Image.open(img_src_path).resize((size,size)).save(img_dst_path)
-            except OSError as e:
-                print(e, ' < ', img_src_path)
-        pbar.close()
+
+        label_images = list(map(str, list(os.listdir(f'{src}/{label}'))))
+        random.shuffle(label_images)
+        split_index = int(len(label_images)*train_ratio)
+        images.extend([f'{dst}/train/{label}/{image}' for image in label_images[:split_index]])
+        images.extend([f'{dst}/test/{label}/{image}' for image in label_images[split_index:]])
+
+        os.makedirs(f'{dst}/train/{label}', exist_ok=True)
+        os.makedirs(f'{dst}/test/{label}', exist_ok=True)
+
+    pbar = tqdm(images)
+    for image in pbar:
+        image_org = f'{src}/{image.split("/",2)[2]}'
+        try:
+            pbar.set_postfix(org=image_org, dst=image)
+            Image.open(image_org).resize((size,size)).save(image)
+
+        except OSError as e:
+            print(e, ' :: ', image_org)
+    pbar.close()
 
 
 if __name__ == '__main__':
     args = get_args()
-    resize(args.input, args.output, args.size)
-    print(args)
+    prepare_train_test_images(args.input, args.output, args.size)
