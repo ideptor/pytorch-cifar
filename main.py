c6a3a12900bd334504483b3dfa14a382f9a9f57c
Suhyun Kim <ideptor@gmail.com>
2023-05-25 00:11:47 +0900

    adjust for 128x128

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
+from torchvision.datasets import ImageFolder
 
 import torchvision
 import torchvision.transforms as transforms
@@ -96,10 +97,11 @@ if __name__ == '__main__':
     # Data
     print('==> Preparing data..')
     transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
+        # transforms.RandomCrop(32, padding=4),
+        # transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+
     ])
 
     transform_test = transforms.Compose([
@@ -107,22 +109,25 @@ if __name__ == '__main__':
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
-    trainset = torchvision.datasets.CIFAR10(
-        root='./data', train=True, download=True, transform=transform_train)
+    # trainset = torchvision.datasets.CIFAR10(
+    #     root='./data', train=True, download=True, transform=transform_train)
+    trainset = ImageFolder("dataset/train", transform=transform_train)
     trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=128, shuffle=True, num_workers=2)
+        trainset, batch_size=16, shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(
-        root='./data', train=False, download=True, transform=transform_test)
+    # testset = torchvision.datasets.CIFAR10(
+    #     root='./data', train=False, download=True, transform=transform_test)
+    testset = ImageFolder("dataset/train", transform=transform_test)
     testloader = torch.utils.data.DataLoader(
-        testset, batch_size=100, shuffle=False, num_workers=2)
+        testset, batch_size=16, shuffle=False, num_workers=2)
 
-    classes = ('plane', 'car', 'bird', 'cat', 'deer',
-            'dog', 'frog', 'horse', 'ship', 'truck')
+    # classes = ('plane', 'car', 'bird', 'cat', 'deer',
+    #         'dog', 'frog', 'horse', 'ship', 'truck')
+    classes = trainset.classes
 
     # Model
     print('==> Building model..')
-    net = ResNet18()
+    net = ResNet18(len(classes))
 
     net = net.to(device)
     if device == 'cuda':
--- models/resnet.py
+++ models/resnet.py
@@ -82,7 +82,8 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        # self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Linear(8192*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -104,8 +105,8 @@ class ResNet(nn.Module):
         return out
 
 
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+def ResNet18(num_classes: int):
+    return ResNet(BasicBlock, [2, 2, 2, 2], num_classes)
 
 
 def ResNet34():
